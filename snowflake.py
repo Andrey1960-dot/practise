56b0183d1697f4807a9870bd55131875f5f407cb
Andrey1960-dot <andreyxiomi5plus@gmail.com>
2024-11-04 18:15:53 +0300

    Add files via upload

---
+++ snowflake.py
@@ -0,0 +1,61 @@
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+# познакомиться с параметрами библиотечной функции рисования снежинки sd.snowflake()
+
+point_0 = sd.get_point(600, 500)
+
+y = 500
+x = 100
+
+sd.snowflake(center=point_0, length=200, factor_a=0.5)
+
+# реализовать падение одной снежинки
+
+while True:
+    sd.clear_screen()
+    point = sd.get_point(x, y)
+    sd.snowflake(center=point, length=50)
+    y -= 50
+    if y < 50:
+        break
+    x2 = 1.3
+
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+
+        break
+
+y = 500
+x = 100
+
+y2 = 450
+x2 = 150
+
+while True:
+    sd.clear_screen()
+    point = sd.get_point(x, y)
+    sd.snowflake(center = point, length = 50)
+    y -= 10
+    if y < 50:
+        break
+    x = x + 10
+
+    point2 = sd.get_point(x2, y2)
+    sd.snowflake(center = point2, length = 30)
+    y2 -= 10
+    if y2 < 50:
+        break
+    x2 = x2 + 20
+
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+# реализовать падение одной снежинки из произвольного места экрана
+# реализовать падение одной снежинки с ветром - смещение в сторону
+
+sd.pause()
+
+
+
